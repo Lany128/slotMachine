cac3ee3dacb6baf919592233284e4fb31ecced48
Evora <lanyevora@gmail.com>
2024-09-24 21:41:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace slotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNumbers = new int[3];
+            
+            Random random = new Random();
+
+            for (int i = 0; i <randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = random.Next(3,3);
+
+            }
+
+            Console.WriteLine("The slot machine result:");
+        }
+    }
+    
+}
